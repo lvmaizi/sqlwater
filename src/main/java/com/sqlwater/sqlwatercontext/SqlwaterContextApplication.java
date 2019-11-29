e1fc45d650ac4529d8618fe95349f54a8930c2bc
fanwenhao <wenhao.fan@yaoxinhd.com>
2019-11-29 16:38:08 +0800

    init project

---
+++ src/main/java/com/sqlwater/sqlwatercontext/SqlwaterContextApplication.java
@@ -0,0 +1,13 @@
+package com.sqlwater.sqlwatercontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlwaterContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlwaterContextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sqlwater/sqlwatercontext/SqlwaterContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlwater.sqlwatercontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlwaterContextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
